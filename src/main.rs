8d97dc73f7549ad74a8df2b95f5b7327f3bcef31
Yest Izit <hi@dmitry.email>
2022-11-26 11:25:38 +0400

    feat: try with inline query handler again

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::env;
 use std::fmt::Debug;
-use std::process::Command;
 
 use teloxide::dispatching::update_listeners::webhooks;
 use teloxide::dispatching::{Dispatcher, UpdateFilterExt};
 use teloxide::error_handlers::LoggingErrorHandler;
-use teloxide::prelude::Message;
-use teloxide::requests::{Requester, ResponseResult};
-use teloxide::types::Update;
-use teloxide::{dptree, Bot};
+use teloxide::requests::{Request, Requester, ResponseResult};
+use teloxide::types::{
+    InlineQuery, InlineQueryResult, InlineQueryResultArticle, InputMessageContent,
+    InputMessageContentText, Update,
+};
+use teloxide::{dptree, respond, Bot};
 use url::Url;
+
+use crate::swastikas::SWASTIKAS;
 
 mod swastikas;
 
@@ -19,9 +22,37 @@ pub(crate) enum PollingMode {
     Webhook,
 }
 
-async fn answer(bot: Bot, msg: Message) -> ResponseResult<()> {
-    bot.send_dice(msg.chat.id).await?;
-    Ok(())
+async fn swastika_answer(bot: Bot, q: InlineQuery) -> ResponseResult<()> {
+    let today_at_midnight = match chrono::Utc::now()
+        .date_naive()
+        .and_hms_milli_opt(0, 0, 0, 0)
+    {
+        Some(t) => t,
+        None => {
+            log::error!("Failed to get today at midnight");
+            panic!("Failed to get today at midnight");
+        }
+    };
+    let timestamp = today_at_midnight.timestamp() as u64;
+    let user_id = q.from.id.0;
+    let random_index = ((user_id + timestamp) as usize) % SWASTIKAS.len();
+    let swastika_text = match SWASTIKAS.get(random_index) {
+        Some(swastika) => swastika.to_string(),
+        None => panic!("No swastika found for index {}", random_index),
+    };
+
+    let swastika_result = InlineQueryResultArticle::new(
+        "01".to_string(),
+        "Какая ты сегодня свастика?",
+        InputMessageContent::Text(InputMessageContentText::new(swastika_text)),
+    );
+    let results = vec![InlineQueryResult::Article(swastika_result)];
+
+    let response = bot.answer_inline_query(&q.id, results).send().await;
+    if let Err(err) = response {
+        log::error!("Error in handler: {:?}", err);
+    }
+    respond(())
 }
 
 #[tokio::main]
@@ -39,7 +70,7 @@ async fn main() {
         Err(_) => panic!("POLLING_MODE env var is not set, probably..."),
     };
 
-    let handler = Update::filter_message().branch(dptree::endpoint(answer));
+    let handler = Update::filter_inline_query().branch(dptree::endpoint(swastika_answer));
 
     match polling_mode {
         PollingMode::Polling => {
