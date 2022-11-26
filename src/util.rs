02030e0f2cdfbd3fba7ae8f1d0d006e4509ed0ce
Yest Izit <hi@dmitry.email>
2022-11-26 13:28:41 +0400

    fix: fix same result for same users

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,7 @@
 use rand::seq::SliceRandom;
 use rand::SeedableRng;
 use rand_pcg::Pcg32;
+use teloxide::payloads::AnswerInlineQuerySetters;
 use teloxide::requests::{Requester, ResponseResult};
 use teloxide::types::{
     InlineQuery, InlineQueryResult, InlineQueryResultArticle, InputMessageContent,
@@ -30,11 +31,6 @@ pub(crate) fn get_random_swastika(user_id: u64) -> String {
 
     let mut rng = Pcg32::seed_from_u64(user_id + today_timestamp);
 
-    log::info!("Today's timestamp: {}", today_timestamp);
-    log::info!("User ID: {}", user_id);
-    log::info!("Seed: {}", user_id + today_timestamp);
-    log::info!("RNG: {:#?}", rng);
-
     match SWASTIKAS.choose(&mut rng) {
         Some(s) => s.to_string(),
         None => panic!("Failed to get swastika"),
@@ -42,19 +38,21 @@ pub(crate) fn get_random_swastika(user_id: u64) -> String {
 }
 
 pub(crate) async fn answer(bot: Bot, q: InlineQuery) -> ResponseResult<()> {
-    let full_name = q.from.full_name();
-    log::info!("Received inline query from {}", full_name);
-
     let swastika_text = get_random_swastika(q.from.id.0);
 
     let swastika_result = InlineQueryResultArticle::new(
-        rand::random::<u32>().to_string(),
+        "0".to_string(),
         "Какая ты сегодня свастика?",
         InputMessageContent::Text(InputMessageContentText::new(swastika_text)),
-    );
+    )
+    .description("Из стихотворения Ярослава Могутина");
+
     let results = vec![InlineQueryResult::Article(swastika_result)];
 
-    let response = bot.answer_inline_query(&q.id, results).await;
+    let response = bot
+        .answer_inline_query(&q.id, results)
+        .is_personal(true)
+        .await;
     if let Err(err) = response {
         log::error!("Error in handler: {:?}", err);
     }
