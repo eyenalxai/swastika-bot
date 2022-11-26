58ce4fda2fbcd3a7bac5cc00e5f21cde677d7381
Yest Izit <hi@dmitry.email>
2022-11-26 11:00:06 +0400

    debug: oof #5

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,10 @@ use std::env;
 use std::fmt::Debug;
 
 use teloxide::dispatching::update_listeners::webhooks;
-use teloxide::dispatching::{Dispatcher, UpdateFilterExt};
 use teloxide::prelude::Message;
-use teloxide::requests::{Request, Requester};
-use teloxide::types::{
-    InlineQuery, InlineQueryResult, InlineQueryResultArticle, InputMessageContent,
-    InputMessageContentText, Update,
-};
-use teloxide::{dptree, respond, Bot};
+use teloxide::requests::Requester;
+use teloxide::Bot;
 use url::Url;
-
-use crate::swastikas::SWASTIKAS;
 
 mod swastikas;
 
@@ -28,41 +21,6 @@ async fn main() {
 
     let bot = Bot::from_env();
 
-    let swastika_handler = |bot: Bot, q: InlineQuery| async move {
-        let today_at_midnight = match chrono::Utc::now()
-            .date_naive()
-            .and_hms_milli_opt(0, 0, 0, 0)
-        {
-            Some(t) => t,
-            None => {
-                log::error!("Failed to get today at midnight");
-                panic!("Failed to get today at midnight");
-            }
-        };
-        let timestamp = today_at_midnight.timestamp() as u64;
-        let user_id = q.from.id.0;
-        let random_index = ((user_id + timestamp) as usize) % SWASTIKAS.len();
-        let swastika_text = match SWASTIKAS.get(random_index) {
-            Some(swastika) => swastika.to_string(),
-            None => panic!("No swastika found for index {}", random_index),
-        };
-
-        let swastika_result = InlineQueryResultArticle::new(
-            "02".to_string(),
-            "Какая ты сегодня свастика?",
-            InputMessageContent::Text(InputMessageContentText::new(swastika_text)),
-        );
-        let results = vec![InlineQueryResult::Article(swastika_result)];
-
-        let response = bot.answer_inline_query(&q.id, results).send().await;
-        if let Err(err) = response {
-            log::error!("Error in handler: {:?}", err);
-        }
-        respond(())
-    };
-
-    let handler = Update::filter_inline_query().branch(dptree::endpoint(swastika_handler));
-
     let polling_mode = match env::var("POLLING_MODE") {
         Ok(mode) => match mode.as_str() {
             "POLLING" => PollingMode::Polling,
@@ -75,11 +33,11 @@ async fn main() {
     match polling_mode {
         PollingMode::Polling => {
             log::info!("Starting polling");
-            Dispatcher::builder(bot, handler)
-                .enable_ctrlc_handler()
-                .build()
-                .dispatch()
-                .await;
+            teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+                bot.send_dice(msg.chat.id).await?;
+                Ok(())
+            })
+            .await;
         }
         PollingMode::Webhook => {
             let port: u16 = env::var("PORT")
@@ -106,7 +64,7 @@ async fn main() {
             teloxide::repl_with_listener(
                 bot,
                 |bot: Bot, msg: Message| async move {
-                    bot.send_message(msg.chat.id, "pong").await?;
+                    bot.send_dice(msg.chat.id).await?;
                     Ok(())
                 },
                 listener,
